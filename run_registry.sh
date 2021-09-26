369bd5e53ad932d74910981de47e5e21502c3cd4
lastkrick <lastkrick@gmail.com>
2021-09-27 02:41:50 +0300

    Update run_registry.sh

--- run_registry.sh
+++ run_registry.sh
@@ -2,6 +2,7 @@
   --restart=always \
   --name registry \
   -v /root/certs:/certs \
+  -v /data:/var/lib/registry
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/MyCertificate.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/MyKey.key \
